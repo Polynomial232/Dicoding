94f02519378361606af47df77f133e87befada48
polynomial232 <daffadhiya232@gmail.com>
2023-03-29 16:52:45 +0700

    backend done

---
+++ javascript-backend-pemula/notes-app-back-end/.eslintrc.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+    }
+}
